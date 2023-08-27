d54c4460685ad22c96a0e5b9d86dfd8013c4e464
Abdul Jaweed <jdgaming7320@gmail.com>
2023-08-27 14:56:21 +0530

    updating

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-model = load_model('BrainTumor10Epochs.h5')
+model = load_model("BrainTumor10Epochs.h5")
 # print('Model loaded. Check http://127.0.0.1:5000/')
+
 
 def get_className(classNo):
     if classNo == 0:
@@ -18,11 +19,12 @@ def get_className(classNo):
     elif classNo == 1:
         return "Yes Brain Tumor"
 
+
 def getResult(img):
     # Check if the file exists before attempting to access it
     if os.path.exists(img):
         image = cv2.imread(img)
-        image = Image.fromarray(image, 'RGB')
+        image = Image.fromarray(image, "RGB")
         image = image.resize((64, 64))
         image = np.array(image)
         input_img = np.expand_dims(image, axis=0)
@@ -32,22 +34,25 @@ def getResult(img):
         # Handle the case where the file does not exist
         return None
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["GET", "POST"])
 def upload():
-    if request.method == 'POST':
-        f = request.files['file']
+    if request.method == "POST":
+        f = request.files["file"]
 
         basepath = os.path.dirname(__file__)
-        file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+        file_path = os.path.join(basepath, "uploads", secure_filename(f.filename))
         f.save(file_path)
         value = getResult(file_path)
-        result = get_className(value) 
+        result = get_className(value)
         return result
     return None
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
--- mainTest.py
+++ mainTest.py
@@ -6,10 +6,10 @@ from keras.models import load_model
 from keras.preprocessing import image
 
 # Load the pre-trained model
-model = load_model('BrainTumor10Epochs.h5')
+model = load_model("BrainTumor10Epochs.h5")
 
 # Load and preprocess the image
-img = cv2.imread('D:\\Tumor_Classification\\pred\\pred0.jpg')
+img = cv2.imread("D:\\Tumor_Classification\\pred\\pred0.jpg")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB format
 img = cv2.resize(img, (64, 64))  # Resize the image to match the model's input size
 img = img / 255.0  # Normalize pixel values to the range [0, 1]
@@ -18,10 +18,10 @@ input_img = np.expand_dims(img, axis=0)  # Add batch dimension
 # Make predictions
 predictions = model.predict(input_img)
 
-'''# Get the class with the highest probability
+"""# Get the class with the highest probability
 predicted_class = np.argmax(predictions)
 
-print("Predicted Class:", predicted_class)'''
+print("Predicted Class:", predicted_class)"""
 # Now, let's make predictions and print the results
 
 # Make predictions on the test set
@@ -34,4 +34,3 @@ print("Binary Predictions:", binary_predictions)
 
 # Print the original probabilities
 print("Probabilities:", predictions)
-
